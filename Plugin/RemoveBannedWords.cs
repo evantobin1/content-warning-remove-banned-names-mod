c2b145fa5ce9c5949b0496b384d4c1c2b37ca39f
Evan Tobin <evantobin91384@gmail.com>
2024-04-17 00:52:10 -0700

    initial commit

---
+++ Plugin/RemoveBannedWords.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+
+
+namespace Plugin
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class RemoveBannedWords : BaseUnityPlugin
+    {
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+
+        // Patching a method to modify a private field
+        [HarmonyPatch(typeof(PlayerVisor), "RPCA_SetVisorText")]
+        public class PatchPlayerVisorClass
+        {
+            static bool Prefix(ref string text, ref List<string> ___bannedWords)
+            {
+                ___bannedWords = new List<string>();
+
+                // Return true to run the original method after this prefix
+                return true;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# RemoveBannedWords Mod for Content Warning
+
+## Description
+The **RemoveBannedWords** mod automatically removes all banned character names from the game "Content Warning." It ensures that users can enjoy a more flexible naming convention without encountering restricted name alerts.
+
+## Version
+- **Version**: 1.0.0
+
+## Installation
+
+### Steps
+   - Navigate to the latest version of the **RemoveBannedWords** mod from [thunderstore.io](https://thunderstore.io/c/content-warning/p/evantobin1/RemoveBannedWords/).
+   - Install the mod with the R2 Mod Manager.
+
+### Start the Game
+Run the game normally. If the mod is installed correctly, it will modify the game to remove banned character names automatically.
+
+## Features
+- **Seamless Integration**: Works silently in the background without disrupting gameplay.
+
+Thank you for using **RemoveBannedWords**!
